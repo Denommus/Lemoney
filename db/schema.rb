d3d48ffcb523bff97d862a58918d95f02ff929aa
Yuri Albuquerque <yuridenommus@gmail.com>
2018-01-23 02:31:08 -0200

    Adding support for postgresql.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180122234141) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "offers", force: :cascade do |t|
     t.string "advertiser_name", null: false
     t.string "url", null: false
